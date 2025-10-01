cb4c1552d86c5e026f4eecea6d2184d3bff3146f
JonLarriba <jon.larriba@opendeusto.es>
2025-10-01 09:20:52 +0200

    Just added the new structure for the project.

---
+++ PractiseForSwingAndGit3/src/UI/MainWindowSwingGit.java
@@ -0,0 +1,7 @@
+package UI;
+
+public class MainWindowSwingGit {
+	public static void main(String[] args) {
+		WindowSwingGit window = new WindowSwingGit();
+	}
+}
---
+++ PractiseForSwingAndGit3/src/UI/WindowSwingGit.java
@@ -0,0 +1,20 @@
+package UI;
+
+import javax.swing.JFrame;
+
+public class WindowSwingGit extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	public WindowSwingGit() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Git Good");
+		setSize(640, 480);
+		setLocationRelativeTo(null);
+		
+		
+		
+		setVisible(true);
+	}
+
+}
